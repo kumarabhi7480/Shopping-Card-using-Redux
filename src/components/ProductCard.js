e548fbe336b4eb5ba95b5bdc73963e9327d181c0
Abhishek Kumar <kumarabhi7480>
2023-04-19 05:38:38 +0530

    all update

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,16 +1,22 @@
 import { useDispatch, useSelector } from "react-redux";
 import {add, remove} from "../store/cartSlice";
 import "./ProductCard.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const ProductCard = ({product}) => {
   const dispatch = useDispatch();
   const cartList = useSelector(state => state.cartState.cartList);
-  const {name, price, image} = product;
+  const [isInCart, setIsInCart] = useState(false);
+  const {id, name, price, image} = product;
 
   useEffect(()=>{
-     
-  })
+     const productInCart = cartList.find(item => item.id === id);
+     if(productInCart){
+      setIsInCart(true);
+     }else{
+      setIsInCart(false);
+     }
+  },[cartList, id]);
 
   return (
     <div className="productCard">
@@ -18,7 +24,8 @@ export const ProductCard = ({product}) => {
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        <button onClick={()=>dispatch(add(product))}>Add To Cart</button>
+        {isInCart ?     (<button className="remove" onClick={()=>dispatch(remove(product))}>Remove</button>) :  (<button onClick={()=>dispatch(add(product))}>Add To Cart</button>) }
+
       </div>
     </div>
   )
